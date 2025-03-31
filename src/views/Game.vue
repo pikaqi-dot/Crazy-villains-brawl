5ae03b9ac3059cc56d1846adf90cf87da45b2bd6
pikaqi-dot <hual83129@gmail.com>
2025-03-31 13:26:32 +0800

    feat: 删除不必要的图片和 CSS 文件，更新游戏组件的背景图像样式

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,14 +1,17 @@
 <template>
-    <div ref="gameContainer" class="game-container"></div>
+     <div class="game-container" :style="{ backgroundImage: `url(/assets/${imageName}.png)` }"></div>
 </template>
 
 <script setup lang="ts">
-import config from '../game/scenes/my'
-import { onMounted } from 'vue';
-import Phaser from 'phaser'
+import config from '../game/scenes/my';
+import { onMounted, ref } from 'vue';
+import Phaser from 'phaser';
+
 onMounted(() => {
     // const game = new Phaser.Game(config);
-})
+});
+
+const imageName = ref('background'); // 动态图片名称
 </script>
 
 <style scoped>
@@ -19,7 +22,7 @@ onMounted(() => {
 
 
 .game-container {
-  background-image: url('/assets/background.png'); /* 设置背景图片路径 */
+  /* background-image: url('/assets/background.png'); 设置背景图片路径 */
   background-size: cover; /* 让背景图片按比例缩放铺满整个容器 */
   background-position: center; /* 背景图片居中显示 */
   background-repeat: no-repeat; /* 防止背景图片重复 */
