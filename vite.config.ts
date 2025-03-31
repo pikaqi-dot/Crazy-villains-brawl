6cbea4f0b2cf7dfc075373d6ac0c22c8d4528b5d
pikaqi-dot <hual83129@gmail.com>
2025-03-31 09:52:30 +0800

    feat: 更新 Vite 配置，移除不必要的插件和调整解析扩展   base: './', // 使用相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "tailwindcss";
 import dts from 'vite-plugin-dts';
 export default defineConfig({
-  plugins: [vue(),dts()],
+  plugins: [vue()],
+  base: './', // 使用相对路径
   css: {
     postcss: {
       plugins: [tailwindcss()], // 生成类型声明文件],
     },
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json'], // 优先解析 .ts 文件
+    extensions: ['.ts', '.json'], // 优先解析 .ts 文件
   },
 
 });
