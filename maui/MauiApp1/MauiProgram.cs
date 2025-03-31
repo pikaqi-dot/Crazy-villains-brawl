72f01ac89795a22a2dc042c6678eaef41ba18b2d
pikaqi-dot <hual83129@gmail.com>
2025-03-31 14:05:24 +0800

    feat: 更新项目文件以支持多平台构建，添加 Windows 包类型和平台设置

--- maui/MauiApp1/MauiProgram.cs
+++ maui/MauiApp1/MauiProgram.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.Extensions.Logging;
 
+/**
+目标平台	正确命令	输出文件
+Android (APK)	dotnet build -c Release -f net8.0-android	.apk
+Android (AAB)	dotnet publish -c Release -f net8.0-android /p:AndroidPackageFormat=aab	.aab
+Windows (exe)	dotnet publish -c Release -f net8.0-windows10.0.19041.0 -r win-x64	/p:PublishSingleFile=true .exe
+
+*/
 namespace MauiApp1
 {
     public static class MauiProgram
